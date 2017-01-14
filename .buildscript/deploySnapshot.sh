8d7dfea79466864f569a76dd6e0608e01bc0debd
Jared Burrows <jaredsburrows@gmail.com>
2017-01-13 20:05:46 -0800

    add publishing task, only publish snapshots to artifactory

--- .buildscript/deploySnapshot.sh
+++ .buildscript/deploySnapshot.sh
@@ -13,6 +13,6 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
   echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
 else
   echo "Deploying snapshot..."
-  ./gradlew artifactoryPublish publishPlugins -Dgradle.publish.key=$GRADLE_KEY -Dgradle.publish.secret=$GRADLE_SECRET
+  ./gradlew artifactoryPublish
   echo "Snapshot deployed!"
 fi
