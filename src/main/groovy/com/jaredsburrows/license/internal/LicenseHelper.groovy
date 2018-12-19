3a54691dd04013c5bdf99891216c650bca842e97
Jared Burrows <jaredsburrows@gmail.com>
2018-12-18 18:05:33 -0800

    Revert "Fix for #52 - Eliminate Duplicate Licenses (#54)" (#56)
    This reverts commit 9a8a3a585a021b0ce38dade0321c4698bfc8f571.

--- src/main/groovy/com/jaredsburrows/license/internal/LicenseHelper.groovy
+++ src/main/groovy/com/jaredsburrows/license/internal/LicenseHelper.groovy
@@ -10,98 +10,79 @@ final class LicenseHelper {
   final static def LICENSE_MAP = [
     // Apache License 2.0
     // https://github.com/github/choosealicense.com/blob/gh-pages/_licenses/apache-2.0.txt
-    "Apache 2.0"                                                     : "apache-2.0.txt",
-    "Apache License 2.0"                                             : "apache-2.0.txt",
-    "The Apache Software License"                                    : "apache-2.0.txt",
-    "The Apache Software License, Version 2.0"                       : "apache-2.0.txt",
-    "http://www.apache.org/licenses/LICENSE-2.0.txt"                 : "apache-2.0.txt",
-    "https://www.apache.org/licenses/LICENSE-2.0.txt"                : "apache-2.0.txt",
-    "http://opensource.org/licenses/Apache-2.0"                      : "apache-2.0.txt",
-    "https://opensource.org/licenses/Apache-2.0"                     : "apache-2.0.txt",
-    "http://www.apache.org/licenses/LICENSE-2.0.html"                : "apache-2.0.txt",
-    "https://www.apache.org/licenses/LICENSE-2.0.html"               : "apache-2.0.txt",
-    "http://www.apache.org/licenses/LICENSE-2.0"                     : "apache-2.0.txt",
-    "https://www.apache.org/licenses/LICENSE-2.0"                    : "apache-2.0.txt",
+    "Apache 2.0"                                               : "apache-2.0.txt",
+    "Apache License 2.0"                                       : "apache-2.0.txt",
+    "The Apache Software License"                              : "apache-2.0.txt",
+    "The Apache Software License, Version 2.0"                 : "apache-2.0.txt",
+    "http://www.apache.org/licenses/LICENSE-2.0.txt"           : "apache-2.0.txt",
+    "https://www.apache.org/licenses/LICENSE-2.0.txt"          : "apache-2.0.txt",
+    "http://opensource.org/licenses/Apache-2.0"                : "apache-2.0.txt",
+    "https://opensource.org/licenses/Apache-2.0"               : "apache-2.0.txt",
 
     // BSD 2-Clause "Simplified" License
     // https://github.com/github/choosealicense.com/blob/gh-pages/_licenses/bsd-2-clause.txt
-    "BSD 2-Clause \"Simplified\" License"                            : "bsd-2-clause.txt",
-    "http://opensource.org/licenses/BSD-2-Clause"                    : "bsd-2-clause.txt",
-    "https://opensource.org/licenses/BSD-2-Clause"                   : "bsd-2-clause.txt",
+    "BSD 2-Clause \"Simplified\" License"                      : "bsd-2-clause.txt",
+    "http://opensource.org/licenses/BSD-2-Clause"              : "bsd-2-clause.txt",
+    "https://opensource.org/licenses/BSD-2-Clause"             : "bsd-2-clause.txt",
 
     // BSD 3-Clause "New" or "Revised" License
     // https://github.com/github/choosealicense.com/blob/gh-pages/_licenses/bsd-3-clause.txt
-    "BSD 3-Clause \"New\" or \"Revised\" License"                    : "bsd-3-clause.txt",
-    "http://opensource.org/licenses/BSD-3-Clause"                    : "bsd-3-clause.txt",
-    "https://opensource.org/licenses/BSD-3-Clause"                   : "bsd-3-clause.txt",
+    "BSD 3-Clause \"New\" or \"Revised\" License"              : "bsd-3-clause.txt",
+    "http://opensource.org/licenses/BSD-3-Clause"              : "bsd-3-clause.txt",
+    "https://opensource.org/licenses/BSD-3-Clause"             : "bsd-3-clause.txt",
 
     // Eclipse Public License 2.0
     // https://github.com/github/choosealicense.com/blob/gh-pages/_licenses/epl-2.0.txt
-    "Eclipse Public License 2.0"                                     : "epl-2.0.txt",
-    "http://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.txt"       : "epl-2.0.txt",
-    "https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.txt"      : "epl-2.0.txt",
-    "http://opensource.org/licenses/EPL-2.0"                         : "epl-2.0.txt",
-    "https://opensource.org/licenses/EPL-2.0"                        : "epl-2.0.txt",
+    "Eclipse Public License 2.0"                               : "epl-2.0.txt",
+    "http://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.txt" : "epl-2.0.txt",
+    "https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.txt": "epl-2.0.txt",
+    "http://opensource.org/licenses/EPL-2.0"                   : "epl-2.0.txt",
+    "https://opensource.org/licenses/EPL-2.0"                  : "epl-2.0.txt",
 
     // GNU General Public License v2.0
     // https://github.com/github/choosealicense.com/blob/gh-pages/_licenses/gpl-2.0.txt
-    "GNU General Public License v2.0"                                : "gpl-2.0.txt",
-    "http://www.gnu.org/licenses/gpl-2.0.txt"                        : "gpl-2.0.txt",
-    "https://www.gnu.org/licenses/gpl-2.0.txt"                       : "gpl-2.0.txt",
-    "http://opensource.org/licenses/GPL-2.0"                         : "gpl-2.0.txt",
-    "https://opensource.org/licenses/GPL-2.0"                        : "gpl-2.0.txt",
+    "GNU General Public License v2.0"                          : "gpl-2.0.txt",
+    "http://www.gnu.org/licenses/gpl-2.0.txt"                  : "gpl-2.0.txt",
+    "https://www.gnu.org/licenses/gpl-2.0.txt"                 : "gpl-2.0.txt",
+    "http://opensource.org/licenses/GPL-2.0"                   : "gpl-2.0.txt",
+    "https://opensource.org/licenses/GPL-2.0"                  : "gpl-2.0.txt",
 
     // GNU General Public License v3.0
     // https://github.com/github/choosealicense.com/blob/gh-pages/_licenses/gpl-3.0.txt
-    "GNU General Public License v3.0"                                : "gpl-3.0.txt",
-    "https//www.gnu.org/licenses/gpl-3.0.txt"                        : "gpl-3.0.txt",
-    "https://www.gnu.org/licenses/gpl-3.0.txt"                       : "gpl-3.0.txt",
-    "http://opensource.org/licenses/GPL-3.0"                         : "gpl-3.0.txt",
-    "https://opensource.org/licenses/GPL-3.0"                        : "gpl-3.0.txt",
+    "GNU General Public License v3.0"                          : "gpl-3.0.txt",
+    "https//www.gnu.org/licenses/gpl-3.0.txt"                  : "gpl-3.0.txt",
+    "https://www.gnu.org/licenses/gpl-3.0.txt"                 : "gpl-3.0.txt",
+    "http://opensource.org/licenses/GPL-3.0"                   : "gpl-3.0.txt",
+    "https://opensource.org/licenses/GPL-3.0"                  : "gpl-3.0.txt",
 
     // GNU Lesser General Public License v2.1
     // https://github.com/github/choosealicense.com/blob/gh-pages/_licenses/lgpl-2.1.txt
-    "GNU Lesser General Public License v2.1"                         : "lgpl-2.1.txt",
-    "http://www.gnu.org/licenses/lgpl-2.1.txt"                       : "lgpl-2.1.txt",
-    "https://www.gnu.org/licenses/lgpl-2.1.txt"                      : "lgpl-2.1.txt",
-    "http://opensource.org/licenses/LGPL-2.1"                        : "lgpl-2.1.txt",
-    "https://opensource.org/licenses/LGPL-2.1"                       : "lgpl-2.1.txt",
+    "GNU Lesser General Public License v2.1"                   : "lgpl-2.1.txt",
+    "http://www.gnu.org/licenses/lgpl-2.1.txt"                 : "lgpl-2.1.txt",
+    "https://www.gnu.org/licenses/lgpl-2.1.txt"                : "lgpl-2.1.txt",
+    "http://opensource.org/licenses/LGPL-2.1"                  : "lgpl-2.1.txt",
+    "https://opensource.org/licenses/LGPL-2.1"                 : "lgpl-2.1.txt",
 
     // GNU Lesser General Public License v3.0
     // https://github.com/github/choosealicense.com/blob/gh-pages/_licenses/lgpl-3.0.txt
-    "GNU Lesser General Public License v3.0"                         : "lgpl-3.0.txt",
-    "http://www.gnu.org/licenses/lgpl-3.0.txt"                       : "lgpl-3.0.txt",
-    "https://www.gnu.org/licenses/lgpl-3.0.txt"                      : "lgpl-3.0.txt",
-    "http://opensource.org/licenses/LGPL-3.0"                        : "lgpl-3.0.txt",
-    "https://opensource.org/licenses/LGPL-3.0"                       : "lgpl-3.0.txt",
+    "GNU Lesser General Public License v3.0"                   : "lgpl-3.0.txt",
+    "http://www.gnu.org/licenses/lgpl-3.0.txt"                 : "lgpl-3.0.txt",
+    "https://www.gnu.org/licenses/lgpl-3.0.txt"                : "lgpl-3.0.txt",
+    "http://opensource.org/licenses/LGPL-3.0"                  : "lgpl-3.0.txt",
+    "https://opensource.org/licenses/LGPL-3.0"                 : "lgpl-3.0.txt",
 
     // MIT License
     // https://github.com/github/choosealicense.com/blob/gh-pages/_licenses/mit.txt
-    "MIT License"                                                    : "mit.txt",
-    "MIT"                                                            : "mit.txt",
-    "http://opensource.org/licenses/MIT"                             : "mit.txt",
-    "https://opensource.org/licenses/MIT"                            : "mit.txt",
+    "MIT License"                                              : "mit.txt",
+    "http://opensource.org/licenses/MIT"                       : "mit.txt",
+    "https://opensource.org/licenses/MIT"                      : "mit.txt",
 
     // Mozilla Public License 2.0
     // https://github.com/github/choosealicense.com/blob/gh-pages/_licenses/mpl-2.0.txt
-    "Mozilla Public License 2.0"                                     : "mpl-2.0.txt",
-    "http://www.mozilla.org/media/MPL/2.0/index.txt"                 : "mpl-2.0.txt",
-    "https://www.mozilla.org/media/MPL/2.0/index.txt"                : "mpl-2.0.txt",
-    "http://opensource.org/licenses/MPL-2.0"                         : "mpl-2.0.txt",
-    "https://opensource.org/licenses/MPL-2.0"                        : "mpl-2.0.txt",
-
-    // Creative Commons Zero v1.0 Universal
-    // https://github.com/github/choosealicense.com/blob/gh-pages/_licenses/cc0-1.0.txt
-    "Creative Commons Zero v1.0 Universal"                           : "cc0-1.0.txt",
-    "Creative Commons Zero"                                          : "cc0-1.0.txt",
-    "CCO"                                                            : "cc0-1.0.txt",
-    "https://creativecommons.org/publicdomain/zero/1.0/"             : "cc0-1.0.txt",
-    "http://creativecommons.org/publicdomain/zero/1.0/"              : "cc0-1.0.txt",
-    "https://creativecommons.org/publicdomain/zero/1.0"              : "cc0-1.0.txt",
-    "http://creativecommons.org/publicdomain/zero/1.0"               : "cc0-1.0.txt",
-    "https://creativecommons.org/publicdomain/zero/1.0/legalcode.txt": "cc0-1.0.txt",
-    "http://creativecommons.org/publicdomain/zero/1.0/legalcode.txt" : "cc0-1.0.txt",
-    "https://creativecommons.org/publicdomain/zero/1.0/legalcode"    : "cc0-1.0.txt",
-    "http://creativecommons.org/publicdomain/zero/1.0/legalcode"     : "cc0-1.0.txt"
+    "Mozilla Public License 2.0"                               : "mpl-2.0.txt",
+    "http://www.mozilla.org/media/MPL/2.0/index.txt"           : "mpl-2.0.txt",
+    "https://www.mozilla.org/media/MPL/2.0/index.txt"          : "mpl-2.0.txt",
+    "http://opensource.org/licenses/MPL-2.0"                   : "mpl-2.0.txt",
+    "https://opensource.org/licenses/MPL-2.0"                  : "mpl-2.0.txt"
   ]
 }
