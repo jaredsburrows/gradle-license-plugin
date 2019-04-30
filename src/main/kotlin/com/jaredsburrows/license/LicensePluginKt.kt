919520b687ef1dc2b2a89d60d746a2180ad82e3f
Jared Burrows <jaredsburrows@gmail.com>
2019-04-29 23:02:20 -0400

    Migrate more code to Kotlin (#88)

--- src/main/kotlin/com/jaredsburrows/license/LicensePluginKt.kt
+++ src/main/kotlin/com/jaredsburrows/license/LicensePluginKt.kt
@@ -1,29 +1,34 @@
 package com.jaredsburrows.license
 
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.FeaturePlugin
+import com.android.build.gradle.InstantAppPlugin
 import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.LibraryPlugin
 import com.android.build.gradle.TestExtension
+import com.android.build.gradle.TestPlugin
 import com.android.build.gradle.api.BaseVariant
 import org.gradle.api.DomainObjectCollection
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPlugin
 
 abstract class LicensePluginKt : Plugin<Project> {
-  companion object {
-    /** Handles pre-3.0 and 3.0+, "com.android.base" was added in AGP 3.0 */
-    private val ANDROID_IDS = arrayOf(
-      "com.android.application",
-      "com.android.feature",
-      "com.android.instantapp",
-      "com.android.library",
-      "com.android.test")
-  }
-
   override fun apply(project: Project) {
-    project.plugins.withId("java") { configureJavaProject(project) }
-
-    ANDROID_IDS.forEach { id ->
-      project.plugins.withId(id) { configureAndroidProject(project) }
+    project.plugins.all { plugin ->
+      when (plugin) {
+        is JavaPlugin -> {
+          configureJavaProject(project)
+        }
+        is AppPlugin,
+        is FeaturePlugin,
+        is LibraryPlugin,
+        is InstantAppPlugin,
+        is TestPlugin -> {
+          configureAndroidProject(project)
+        }
+      }
     }
   }
 
