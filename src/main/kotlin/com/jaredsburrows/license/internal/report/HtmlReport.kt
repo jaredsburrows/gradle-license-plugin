08afe755168493f0dda3e5fea1c7488ae7a13034
DonnKey <donnterry@gmail.com>
2019-10-16 18:59:06 -0700

    Handle copyright, multiple licenses in html output.

--- src/main/kotlin/com/jaredsburrows/license/internal/report/HtmlReport.kt
+++ src/main/kotlin/com/jaredsburrows/license/internal/report/HtmlReport.kt
@@ -1,25 +1,10 @@
 package com.jaredsburrows.license.internal.report
 
 import com.jaredsburrows.license.internal.LicenseHelper
+import com.jaredsburrows.license.internal.pom.License
 import com.jaredsburrows.license.internal.pom.Project
-import kotlinx.html.A
-import kotlinx.html.FlowOrInteractiveOrPhrasingContent
-import kotlinx.html.HtmlTagMarker
-import kotlinx.html.a
-import kotlinx.html.attributesMapOf
-import kotlinx.html.body
-import kotlinx.html.h3
-import kotlinx.html.head
-import kotlinx.html.html
-import kotlinx.html.li
-import kotlinx.html.pre
+import kotlinx.html.*
 import kotlinx.html.stream.appendHTML
-import kotlinx.html.style
-import kotlinx.html.title
-import kotlinx.html.ul
-import kotlinx.html.unsafe
-import kotlinx.html.visit
-import java.io.File
 
 /**
  * Generates HTML report of projects dependencies.
@@ -33,6 +18,10 @@ class HtmlReport(private val projects: List<Project>) {
     const val NO_LIBRARIES = "None"
     const val NO_LICENSE = "No license found"
     const val NOTICE_LIBRARIES = "Notice for packages:"
+    const val COPYRIGHT = "Copyright "
+    const val DEFAULT_AUTHOR = "The original author or authors"
+    const val DEFAULT_YEAR = "20xx"
+    const val MISSING_LICENSE = "Missing standard license text for: "
   }
 
   /** Return Html as a String. */
@@ -43,21 +32,23 @@ class HtmlReport(private val projects: List<Project>) {
     val projectsMap = hashMapOf<String?, List<Project>>()
     val licenseMap = LicenseHelper.licenseMap
 
-    // Store packages by license
+    // Store packages by licenses: build a composite key of all the licenses, sorted in the (probably vain)
+    // hope that there's more than one project with the same set of multiple licenses.
     projects.forEach { project ->
-      var key: String? = ""
+      val keys: MutableList<String> = mutableListOf()
 
-      // first check to see if the project's license is in our list of known licenses:
+      // first check to see if the project's license is in our list of known licenses.
       if (!project.licenses.isNullOrEmpty()) {
-        val license = project.licenses.first()
-        key = when {
-          // look up by url
-          licenseMap.containsKey(license.url) -> licenseMap[license.url]
-          // then by name
-          licenseMap.containsKey(license.name) -> licenseMap[license.name]
-          // otherwise, use the url as a key
-          else -> license.url
+        project.licenses.forEach { license ->
+          keys.add(getLicenseKey(license))
         }
+      }
+
+      keys.sortedWith(compareBy(String.CASE_INSENSITIVE_ORDER) { it })
+      var key = ""
+      if (keys.size > 0) {
+        // No Licenses -> empty key, sort first
+        key = keys.toString()
       }
 
       if (!projectsMap.containsKey(key)) {
@@ -100,36 +91,82 @@ class HtmlReport(private val projects: List<Project>) {
                 li {
                   a(href = "#$currentLicense") {
                     +project.name
+                    +" ("
+                    +project.version
+                    +")"
+                  }
+                  val copyrightYear = if (project.year.isEmpty()) DEFAULT_YEAR else project.year
+                  dl {
+                    if (project.developers.isNotEmpty()) {
+                      project.developers.forEach { developer ->
+                        dt {
+                          +COPYRIGHT
+                          +Entities.copy
+                          +" "
+                          +copyrightYear
+                          +" "
+                          +developer.name
+                        }
+                      }
+                    } else {
+                      dt {
+                        +COPYRIGHT
+                        +Entities.copy
+                        +" "
+                        +copyrightYear
+                        +" "
+                        +DEFAULT_AUTHOR
+                      }
+                    }
                   }
                 }
               }
 
+              // This isn't correctly indented in the html source (but is otherwise correct).
+              // It appears to be a bug in the DSL implementation from what little I see on the web.
               a(name = currentLicense.toString())
-              // Display associated license with libraries
-              if (currentProject?.licenses.isNullOrEmpty()) {
+
+              // Display associated license text with libraries
+              val licenses = currentProject?.licenses
+              if (licenses.isNullOrEmpty()) {
                 pre {
                   unsafe { +NO_LICENSE }
                 }
-              } else if (!entry.key.isNullOrEmpty() && licenseMap.values.contains(entry.key!!)) {
-                // license from license map
-                pre {
-                  unsafe { +getLicenseText(entry.key!!) }
-                }
               } else {
-                // if not found in the map, just display the info from the POM.xml
-                val currentLicenseName = currentProject?.licenses?.first()?.name?.trim()
-                val currentUrl = currentProject?.licenses?.first()?.url?.trim()
+                licenses.forEach { license ->
+                  val key = getLicenseKey(license)
+                  if (key.isNotEmpty() && licenseMap.values.contains(key)) {
+                    // license from license map
+                    pre {
+                      unsafe { +getLicenseText(key) }
+                    }
+                  } else {
+                    // if not found in the map, just display the info from the POM.xml
+                    val currentLicenseName = license.name.trim()
+                    val currentUrl = license.url.trim()
 
-                if (!currentLicenseName.isNullOrEmpty() || !currentUrl.isNullOrEmpty()) {
-                  pre {
-                    unsafe { +"$currentLicenseName\n<a href=\"$currentUrl\">$currentUrl</a>" }
+                    if (currentLicenseName.isNotEmpty() && currentUrl.isNotEmpty()) {
+                      pre {
+                        unsafe { +"$currentLicenseName\n<a href=\"$currentUrl\">$currentUrl</a>" }
+                      }
+                    } else if (currentUrl.isNotEmpty()) {
+                      pre {
+                        unsafe { +"<a href=\"$currentUrl\">$currentUrl</a>" }
+                      }
+                    } else if (currentLicenseName.isNotEmpty()) {
+                      pre {
+                        unsafe { +"$currentLicenseName\n" }
+                      }
+                    } else {
+                      pre {
+                        unsafe { +NO_LICENSE }
+                      }
+                    }
                   }
-                } else {
-                  pre {
-                    unsafe { +NO_LICENSE }
-                  }
+                  br
                 }
               }
+              hr {}
             }
           }
         }
@@ -157,10 +194,23 @@ class HtmlReport(private val projects: List<Project>) {
       }
     }.toString()
 
-  private fun getLicenseText(fileName: String): String =
-    HtmlReport::class.java.getResource(
-      "/license/$fileName".replace('/', File.separatorChar)
-    ).readText()
+  private fun getLicenseText(fileName: String): String {
+    val resource = HtmlReport::class.java.getResource("/license/$fileName")
+    return (resource?.readText() ?: MISSING_LICENSE + fileName)
+  }
+
+  private fun getLicenseKey(license: License): String {
+    // See if the license is in our list of known licenses (which coalesces differing URLs to the same license text)
+    // If not, use the URL if present. Else "".
+    return when {
+      // look up by url
+      LicenseHelper.licenseMap.containsKey(license.url) -> LicenseHelper.licenseMap[license.url]
+      // then by name
+      LicenseHelper.licenseMap.containsKey(license.name) -> LicenseHelper.licenseMap[license.name]
+      // otherwise, use the url as a key
+      else -> license.url
+    } as String
+  }
 }
 
 @HtmlTagMarker
