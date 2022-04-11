24e229fd35ad723a1e3f65acf994e2a6affe5113
Jared Burrows <jaredsburrows@gmail.com>
2022-04-10 20:47:03 -0400

    optimizing configuring project

---
+++ src/main/kotlin/com/jaredsburrows/license/BaseLicenseReportTask.kt
@@ -0,0 +1,49 @@
+package com.jaredsburrows.license
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
+import java.io.File
+
+/** A [org.gradle.api.Task] that is common for both Java and Android projects. */
+open class BaseLicenseReportTask : DefaultTask() {
+
+  // Task annotations cannot be internal
+  @OutputFile var csvFile: File
+  @OutputFile var htmlFile: File
+  @OutputFile var jsonFile: File
+  @Input var generateCsvReport = false
+  @Input var generateHtmlReport = false
+  @Input var generateJsonReport = false
+  @Input var copyCsvReportToAssets = false
+  @Input var copyHtmlReportToAssets = false
+  @Input var copyJsonReportToAssets = false
+  private var outputPath: String
+
+  init {
+    // From DefaultTask
+    description = "Outputs licenses report for $name."
+    group = "Reporting"
+
+    // Customizing internal task options
+    outputPath = "${project.buildDir}/reports/licenses/".replace('/', File.separatorChar)
+    csvFile = File(outputPath, "$name$CSV_EXT")
+    htmlFile = File(outputPath, "$name$HTML_EXT")
+    jsonFile = File(outputPath, "$name$JSON_EXT")
+
+    // Customizing internal task options from extension
+    val extension = project.extensions.getByType(LicenseReportExtension::class.java)
+    generateCsvReport = extension.generateCsvReport
+    generateHtmlReport = extension.generateHtmlReport
+    generateJsonReport = extension.generateJsonReport
+    copyCsvReportToAssets = extension.copyCsvReportToAssets
+    copyHtmlReportToAssets = extension.copyHtmlReportToAssets
+    copyJsonReportToAssets = extension.copyJsonReportToAssets
+  }
+
+  internal companion object {
+    const val CSV_EXT = ".csv"
+    const val HTML_EXT = ".html"
+    const val JSON_EXT = ".json"
+  }
+}
