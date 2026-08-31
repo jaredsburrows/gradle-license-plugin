b895b69d0907de0346d9cd686324b650c8f58132
Jared Burrows <jaredsburrows@gmail.com>
2026-08-31 10:28:23 -0400

    Type-check LicenseReportExtensionSpec and the two plugin specs

--- gradle-license-plugin/src/test/groovy/com/jaredsburrows/license/LicenseReportExtensionSpec.groovy
+++ gradle-license-plugin/src/test/groovy/com/jaredsburrows/license/LicenseReportExtensionSpec.groovy
@@ -1,32 +1,51 @@
 package com.jaredsburrows.license
 
+import groovy.transform.TypeChecked
 import spock.lang.Specification
 
+@TypeChecked
 final class LicenseReportExtensionSpec extends Specification {
-  private LicenseReportExtension extension = new LicenseReportExtension()
-
   def 'the full JSON report is opt in'() {
+    given:
+    LicenseReportExtension extension = new LicenseReportExtension()
+    boolean generateJsonFullReport = extension.generateJsonFullReport
+    boolean copyJsonFullReportToAssets = extension.copyJsonFullReportToAssets
+
     expect: 'the license texts make it large, so it is off until asked for'
-    !extension.generateJsonFullReport
-    !extension.copyJsonFullReportToAssets
+    !generateJsonFullReport
+    !copyJsonFullReportToAssets
   }
 
   def 'the existing report defaults are unchanged'() {
+    given:
+    LicenseReportExtension extension = new LicenseReportExtension()
+    boolean generateCsvReport = extension.generateCsvReport
+    boolean generateHtmlReport = extension.generateHtmlReport
+    boolean generateJsonReport = extension.generateJsonReport
+    boolean generateTextReport = extension.generateTextReport
+    boolean copyHtmlReportToAssets = extension.copyHtmlReportToAssets
+    boolean copyCsvReportToAssets = extension.copyCsvReportToAssets
+    boolean copyJsonReportToAssets = extension.copyJsonReportToAssets
+    boolean copyTextReportToAssets = extension.copyTextReportToAssets
+    boolean useVariantSpecificAssetDirs = extension.useVariantSpecificAssetDirs
+    boolean showVersions = extension.showVersions
+    boolean noIgnoredPatterns = extension.ignoredPatterns.isEmpty()
+
     expect:
-    extension.generateCsvReport
-    extension.generateHtmlReport
-    extension.generateJsonReport
-    extension.generateTextReport
+    generateCsvReport
+    generateHtmlReport
+    generateJsonReport
+    generateTextReport
 
     and: 'only the HTML report is copied to the assets by default'
-    extension.copyHtmlReportToAssets
-    !extension.copyCsvReportToAssets
-    !extension.copyJsonReportToAssets
-    !extension.copyTextReportToAssets
+    copyHtmlReportToAssets
+    !copyCsvReportToAssets
+    !copyJsonReportToAssets
+    !copyTextReportToAssets
 
     and:
-    !extension.useVariantSpecificAssetDirs
-    !extension.showVersions
-    extension.ignoredPatterns.isEmpty()
+    !useVariantSpecificAssetDirs
+    !showVersions
+    noIgnoredPatterns
   }
 }
